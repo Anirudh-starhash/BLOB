cfc8c9e85d7651636cc554d0eb1b7d672e809d7f
Anirudh Pabbaraju <anirudhpabbaraju1103@gmail.com>
2024-11-29 01:04:19 +0530

    x

--- BLOB-BACKEND/app.py
+++ BLOB-BACKEND/app.py
@@ -112,5 +112,6 @@ with app.app_context():
     db.create_all()
     
 if __name__=="__main__":
-    app.run(debug=True)
+    port = os.getenv("PORT", 5000)  # Render sets this automatically
+    app.run(debug=True,host="0.0.0.0",port=int(port))
     
